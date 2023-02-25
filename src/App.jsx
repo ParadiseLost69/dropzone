f887ebede163ff0c10f60a35cd31ad601b1ce986
Teddy <tedloukas12@gmail.com>
2023-02-25 15:57:47 -0500

    Update uploads, add drop-zone, add multiple file input on box

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,75 @@
 import "./App.css";
-
+import React from "react";
 function App() {
   const title = document.head.querySelector("title");
   title.textContent = "Application";
+
+  const [files, setFiles] = React.useState([]);
+
+  function handleFileUpload(e) {}
+
+  function handleDragOver(e) {
+    e.preventDefault();
+  }
+
+  function handleDrop(e) {
+    e.preventDefault();
+    const data = e.dataTransfer.files[0];
+    console.log(e.dataTransfer.items);
+    if (files.includes(data)) {
+      return;
+    }
+    setFiles((prevFiles) => {
+      return [...prevFiles, data];
+    });
+
+    console.log(files);
+  }
+
+  function handleSubmit() {
+    setFiles([]);
+  }
+
   return (
     <div className="App">
-      <h1 className="title">Dropzone</h1>
-
-      <form onDragEnter={(e) => console.log(e)} className="form">
-        <input className="form__upload" type="file" name="" id="" />
-        <label id="label-file-upload" htmlFor="input-file-upload"></label>
-        <button className="upload-button" type="">
-          Upload file
-        </button>
+      <h1 className="title">Click the box or drag and drop a file</h1>
+      <form onChange={(e) => console.log(e.target.files)} action="#">
+        <div
+          className="submit-box"
+          onDrop={handleDrop}
+          onDragOver={handleDragOver}
+        >
+          <input
+            style={{}}
+            className="file-input"
+            type="file"
+            multiple
+            name=""
+            id=""
+          />
+        </div>
       </form>
+      <div>
+        {files.length > 0 && (
+          <div>
+            <label htmlFor="custodian" name="custodia">
+              Custodian
+            </label>
+            <input className="custodian" />
+            <div className="file-card--container">
+              {files.map((file, ind) => {
+                return (
+                  <div className="file-card" key={ind}>
+                    <p>{file.name}</p>
+                    <p>{file.size} bytes</p>
+                  </div>
+                );
+              })}
+            </div>
+            <button onClick={handleSubmit}>submit</button>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/FileUpload.jsx
@@ -0,0 +1,84 @@
+import React, { useState, useEffect } from "react";
+import { ProgressBar } from "react-bootstrap";
+
+const FileUpload = () => {
+  const [files, setFiles] = useState([]);
+  const [custodian, setCustodian] = useState("");
+  const [progress, setProgress] = useState(0);
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    const newFiles = [...files, ...e.dataTransfer.files];
+    setFiles(newFiles);
+  };
+
+  const handleFileInput = (e) => {
+    const newFiles = [...files, ...e.target.files];
+    setFiles(newFiles);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setProgress(0);
+    setFiles([]);
+    setCustodian("");
+  };
+
+  useEffect(() => {
+    let intervalId;
+    if (progress < 100) {
+      intervalId = setInterval(() => {
+        setProgress((prevProgress) => prevProgress + 10);
+      }, 1000);
+    } else {
+      clearInterval(intervalId);
+    }
+    return () => clearInterval(intervalId);
+  }, [progress]);
+
+  return (
+    <div
+      className="file-upload"
+      onDrop={handleDrop}
+      onDragOver={(e) => e.preventDefault()}
+    >
+      <div className="dropzone">
+        <div className="text-center">
+          <h5>Drag and drop files here or click to select files</h5>
+        </div>
+        <input type="file" onChange={handleFileInput} multiple />
+      </div>
+      {files.length > 0 && (
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="custodian">Custodian:</label>
+            <input
+              type="text"
+              id="custodian"
+              className="form-control"
+              value={custodian}
+              onChange={(e) => setCustodian(e.target.value)}
+              required
+            />
+          </div>
+          {files.map((file, index) => (
+            <div key={index} className="mb-3">
+              <div className="d-flex justify-content-between align-items-center">
+                <div>
+                  <i className="far fa-file mr-2"></i>
+                  {file.name} ({(file.size / 1024 / 1024).toFixed(2)} MB)
+                </div>
+                <div>{progress < 100 && <ProgressBar now={progress} />}</div>
+              </div>
+            </div>
+          ))}
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      )}
+    </div>
+  );
+};
+
+export default FileUpload;
